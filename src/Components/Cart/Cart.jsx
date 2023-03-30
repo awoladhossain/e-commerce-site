3fe1857545f1bc2caf30bef9634a91024d3e3fc6
Awolad Hossain <awoladh04@gmail.com>
2023-03-30 23:45:50 +0600

    helloo

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = (props) => {
+    const {cart} = props;
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for(let product of cart)
+    {
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = totalPrice*7 / 10;
+    const grandTotal = totalPrice + totalShipping + tax;
+
+
+    return (
+        <div className='cart'>
+              <h4>Order Summary</h4>
+        <p>Selected Item: {cart.length}</p>
+        <p>Total Price: ${totalPrice} </p>
+        <p>Total Shipping: ${totalShipping}</p>
+        <p>Tax: ${tax.toFixed(2)}</p>
+        <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+        </div>
+    );
+};
+
+export default Cart;
