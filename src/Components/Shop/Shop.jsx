616b286e6b78d6cd83f92f6cf1891d1aacd9e10b
Awolad Hossain <awoladh04@gmail.com>
2023-04-03 18:45:25 +0600

    this is why

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -14,12 +15,41 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+
+   useEffect(()=>{
+      const storeCart =  getShoppingCart();
+      const saveCart =[];
+
+      // step 1:get id of the addProduct 
+       for(const id in storeCart)
+       {
+        //step: 2 get prodcut from products state by using id
+        const addedProduct = products.find(product => product.id === id); 
+        if(addedProduct){
+          // step 3: add quantity
+          const quantity = storeCart[id];
+          addedProduct.quantity = quantity;
+          // step 4: add product to saveCart
+          saveCart.push(addedProduct);
+        }
+        // console.log("addedProdcut",addedProduct);
+       }
+      //  step 5: setCart
+       setCart(saveCart)
+   },[products])
+
+
   const handleAddToCaert =(product)=>
   {
     // console.log(product);
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id)
   }
+
+  // https://makeshift-ice.surge.sh/
+
+
 
   return (
     <div className="shop-container">
