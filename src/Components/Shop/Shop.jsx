4885cb879b03ec2d9ffb89e2e6ac177cb5c3f783
Awolad Hossain <awoladh04@gmail.com>
2023-04-04 00:14:46 +0600

    bujlam nah

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -42,7 +42,23 @@ const Shop = () => {
   const handleAddToCaert =(product)=>
   {
     // console.log(product);
-    const newCart = [...cart, product];
+    // const newCart = [...cart, product];
+    let newCart = [];
+
+    // if product doesn't exist in the cart , then set quantity 1
+    // if product exist in the cart, then increase the quantity
+    const exists = cart.find(pd => pd.id === product.id);
+    if(!exists){
+      product.quantity = 1;
+      newCart =[...cart,product];
+    }
+    else
+    {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter(pd => pd.id !== product.id);
+      newCart =[...remaining,exists];
+    }
+
     setCart(newCart);
     addToDb(product.id)
   }
